7468602ab855f81cf338ede75afc137cb8f842e0
tamal <tamal@appscode.com>
2017-09-16 12:29:33 -0700

    added-all

--- main.go
+++ main.go
@@ -5,18 +5,19 @@ import (
 	"fmt"
 	"github.com/appscode/log"
 	"github.com/hashicorp/vault/api"
+	"github.com/tamalsaha/go-oneliners"
 )
 
 func main() {
 	cfg := api.DefaultConfig()
-	jp(cfg)
+	oneliners.FILE(tj(cfg))
 
 	client, err := api.NewClient(cfg)
 	if err != nil {
 		log.Errorln(err)
 	}
 	s, err := client.Auth().Token().LookupSelf()
-	jp(s)
+	oneliners.FILE(tj(s))
 
 	// list enabled auth mechanism
 	amech, err := client.Sys().ListAuth()
@@ -27,6 +28,7 @@ func main() {
 		fmt.Println(k, tj(v))
 	}
 
+	/*
 	// $ vault auth-enable approle
 	err = client.Sys().EnableAuthWithOptions("approle", &api.EnableAuthOptions{
 		Type: "approle",
@@ -34,26 +36,28 @@ func main() {
 	if err != nil {
 		log.Errorln(err)
 	}
+	*/
 
-	roles, err := client.Logical().List("auth/approle/role")
+	roles, err := client.Logical().List("/auth/approle/role")
 	if err != nil {
 		log.Errorln(err)
 	}
-	jp(roles)
+	oneliners.FILE(tj(roles))
 
 	approle, err := client.Logical().Write("auth/approle/role/testrole", map[string]interface{}{
+		"secret_id_ttl":      "10m",
+		"token_num_uses":     "10",
+		"token_ttl":          "20m",
+		"token_max_ttl":      "30m",
+		"secret_id_num_uses": 40,
 	})
 	if err != nil {
 		log.Errorln(err)
 	}
+	oneliners.FILE(tj(approle))
 }
 
 func tj(v interface{}) string {
 	cb, _ := json.MarshalIndent(v, "", "  ")
 	return string(cb)
-}
-
-func jp(v interface{}) {
-	cb, _ := json.MarshalIndent(v, "", "  ")
-	fmt.Println(string(cb))
 }
